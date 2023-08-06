f6eb363108cbc239a15395fc70ab49c7b6a844af
aminmotahari <mohammadaminmotaharinia@gmail.com>
2023-08-06 14:07:29 +0330

    pages put on place but serious stuff remains
    - bg transparency remains
    - other pages repetitive header pattern remains a mystery
    - things that matter now are written, there is more to it

--- landingpage/urls.py
+++ landingpage/urls.py
@@ -5,11 +5,11 @@ app_name = 'landingpage'
 
 urlpatterns = [
     path('', home_view, name='index'),
-    path('portfolio/', portfolio_view, name='portfolio'),
-    #path('', about_view, name='about'),
-    #path('', resume_view, name='resume'),
-    #path('', services_view, name='services'),
-    #path('', portfolio_view, name='portfolio'),
-    #path('', contact_view, name='contact'),
+    path('portfolio_2/', portfolio_view, name='portfolio'),
+    path('about/', about_view, name='about'),
+    path('resume/', resume_view, name='resume'),
+    path('services/', services_view, name='services'),
+    path('portfolio/', portfolio_view2, name='portfolio2'),
+    path('contact/', contact_view, name='contact'),
     
 ]
--- landingpage/views.py
+++ landingpage/views.py
@@ -2,8 +2,29 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_view(request):
-    return render(request, 'index.html')
+    return render(request, 'website/index.html')
 
 
 def portfolio_view(request):
-    return render(request, 'portfolio-details.html')
+    return render(request, 'website/portfolio-details.html')
+
+
+def about_view(request):
+    return render(request, 'website/about.html')
+
+
+def resume_view(request):
+    return render(request, 'website/resume.html')
+
+
+def services_view(request):
+    return render(request, 'website/services.html')
+
+
+def portfolio_view2(request):
+    return render(request, 'website/portfolio.html')
+
+
+def contact_view(request):
+    return render(request, 'website/contact.html')
+
